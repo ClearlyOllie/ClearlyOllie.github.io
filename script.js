73b46cdfc248bad988680f774c82896935fd946d
ClearlyOllie <95652649+ClearlyOllie@users.noreply.github.com>
2023-05-26 20:30:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function showNavMenu() {
   var men = document.getElementById('compactMenu');
+  var bar = document.getElementById('bar');
 
   if (men.style.display === 'none') {
     men.style.display = 'block';
+    bar.style.display = 'block';
   } else {
     men.style.display = 'none';
+    bar.style.display = 'none';
   }
 
 
